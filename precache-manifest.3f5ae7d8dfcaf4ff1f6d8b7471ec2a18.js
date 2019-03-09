a217c51c3daff69a65d30ce4fc9a8b4e4c5a543b
angrylobster <liewminshan@gmail.com>
2019-03-09 23:45:54 +0800

    Updates

---
+++ precache-manifest.3f5ae7d8dfcaf4ff1f6d8b7471ec2a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5fdf7f86495bc64854ab",
+    "url": "/home/static/js/runtime~main.44701174.js"
+  },
+  {
+    "revision": "c8586bb020beaf075c90",
+    "url": "/home/static/js/main.5fe03edd.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/home/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "c8586bb020beaf075c90",
+    "url": "/home/static/css/main.dae000aa.chunk.css"
+  },
+  {
+    "revision": "4fa67e64f3c4c0c78c5ed10874c1c1b8",
+    "url": "/home/index.html"
+  }
+];
